d23497c0869478fe00b77d17988293b8b0b85ecb
Richard Magahiz <frabdango@magahiz.com>
2013-07-20 20:53:48 -0400

    Scripts to clean up broken links from static files

---
+++ maint/moonlit.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+perl -p -i -e 's{      <a href="http://www.moonlit-path.com">Tales from the Moonlit Path</a><br />\n}{}gs' $1
+
---
+++ maint/sondra.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+perl -p -i -e 's{      <a href="http\://www.sondra.net/al/default.htm">Autumn Leaves</a><br />\n}{}gs' $1
+
